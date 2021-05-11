b81656ace2535504e6f93051558ce39ea36e2013
Durai Velan <durai.9.velan@stackroute.in>
2021-05-11 11:23:50 +0530

    Demo

--- src/main/java/com/stackroute/playerapp/controller/PlayerController.java
+++ src/main/java/com/stackroute/playerapp/controller/PlayerController.java
@@ -8,64 +8,65 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.stackroute.playerapp.model.Player;
 import com.stackroute.playerapp.service.PlayerService;
 
-@Controller
+@RestController
 public class PlayerController {
 
 	@Autowired
 	PlayerService playerService;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String getAllPlayers(Model model) {
+	@GetMapping("/")
+	public ResponseEntity<List<Player>> getAllPlayers() {
+		ResponseEntity<List<Player>> response = null;
 		List<Player> playerlist = playerService.getAllPlayers();
-		model.addAttribute("playerlist", playerlist);
-		return "index";
+		response = new ResponseEntity<>(playerlist,HttpStatus.OK);
+		return response;		
 	}
 
-	@RequestMapping(value = "/addPlayer", method = RequestMethod.POST)
-	public String addPlayer(Model model, HttpServletRequest req, HttpServletResponse res) {
+	@PostMapping(value = "/addPlayer")
+	public ResponseEntity<Player> addPlayer(@RequestBody Player newPlayer) {
 	
-		Player newPlayer = new Player();
-		newPlayer.setPlayername(req.getParameter("playername"));
-		newPlayer.setCountry(req.getParameter("country"));
-		newPlayer.setSports(req.getParameter("sports"));
-
-		playerService.addPlayer(newPlayer);
-
-		return "redirect:/";
+		ResponseEntity<Player> response = null;
+		Player addedPlayer = playerService.addPlayer(newPlayer);
+		response = new ResponseEntity<>(addedPlayer,HttpStatus.CREATED);
+		return response;
 	}
 
-	@RequestMapping(value = "/deletePlayer", method = RequestMethod.GET)
-	public String deletePlayer(@RequestParam String playerid) {
+	@DeleteMapping("/deletePlayer/{playerid}")
+	public ResponseEntity<Player> deletePlayer(@PathVariable String playerid) {
 
-		playerService.deletePlayer(Integer.parseInt(playerid));
-		return "redirect:/";
+		ResponseEntity<Player> response = null;
+		Player deletedPlayer = playerService.deletePlayer(Integer.parseInt(playerid));
+		response = new ResponseEntity<>(deletedPlayer,HttpStatus.OK);
+		return response;	
+		
 	}
 
 	@RequestMapping(value = "/getPlayer/{playerid}", method = RequestMethod.GET)
 	public String getPlayerInfo(Model model, @PathVariable("playerid") String playerid) {
 		Player player = playerService.getPlayerById(Integer.parseInt(playerid));
-		model.addAttribute("playerid", player.getPlayerid());
-		model.addAttribute("playername", player.getPlayername());
-		model.addAttribute("country", player.getCountry());
-		model.addAttribute("sports", player.getSports());
 		
-		return "player";
+		return null;
 	}
 
 	
-	@RequestMapping(value = "/updatePlayer", method = RequestMethod.PUT)
-	public String updatePlayer() {
-		return "index";
-	}
+	
 
 }
